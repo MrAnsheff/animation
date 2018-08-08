a3dbbfa6fd69321d0d1e1437b343c803feb38406
MrAnsheff <mransheff86@gmail.com>
2018-08-08 19:30:03 +0400

    With Central Box

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import '../widgets/cat.dart';
 
 class Home extends StatefulWidget {
-    HomeState createState() => HomeState();
+  HomeState createState() => HomeState();
 }
 
 class HomeState extends State<Home> with TickerProviderStateMixin {
   Animation<double> catAnimation;
   AnimationController catController;
 
-  initState(){
+  initState() {
     super.initState();
 
     catController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(seconds: 1),
       vsync: this,
     );
 
@@ -23,31 +23,53 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
         curve: Curves.easeIn,
       ),
     );
-
-    catController.forward();
   }
 
+  onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      catController.reverse();
+    } else if (catController.status == AnimationStatus.dismissed) {
+      catController.forward();
+    }
+  }
 
-  Widget build(context){
+  Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Animation!'),
       ),
-      body: buildAnimation(),
+      body: GestureDetector(
+        child: Center(
+          child: Stack(
+            children: <Widget>[
+              buildCatAnimation(),
+              buildBox(),
+            ],
+          ),
+        ),
+        onTap: onTap,
+      ),
     );
   }
 
-  Widget buildAnimation(){
+  Widget buildCatAnimation() {
     return AnimatedBuilder(
       animation: catAnimation,
-      builder: (context, child){
+      builder: (context, child) {
         return Container(
           child: child,
           margin: EdgeInsets.only(top: catAnimation.value),
         );
-
       },
       child: Cat(),
+    );
+  }
+
+  Widget buildBox() {
+    return Container(
+      height: 200.0,
+      width: 200.0,
+      color: Colors.brown,
     );
   }
 }
